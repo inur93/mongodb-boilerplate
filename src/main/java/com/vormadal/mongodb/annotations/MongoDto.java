4c990914cb80c629705ea05f25d7bab920700448
Rúni Egholm Vørmadal <runivormadal@gmail.com>
2019-02-02 22:10:47 +0100

    added more functionality to baseDao. Now using the benefits of PartialDto annotation to create updateOperations more easily.

--- src/main/java/com/vormadal/mongodb/annotations/MongoDto.java
+++ src/main/java/com/vormadal/mongodb/annotations/MongoDto.java
@@ -8,9 +8,20 @@ import java.lang.annotation.Target;
 /**
  * <p>Created: 31-01-2019</p>
  * <p>author: Runi</p>
+ *
+ * <p>
+ *     When a class is annotated with {@link MongoDto} a static class will be created
+ *     inside the Fields class containing the name of all of this classes fields.
+ * </p>
+ * <p>
+ *     This can be useful when querying or updating a mongo collection.
+ * </p>
  */
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.TYPE)
 public @interface MongoDto {
+    /**
+     * see {@link PartialDto} for more information.
+     */
     PartialDto[] partials() default {};
 }
--- src/main/java/com/vormadal/mongodb/annotations/PartialDto.java
+++ src/main/java/com/vormadal/mongodb/annotations/PartialDto.java
@@ -8,6 +8,22 @@ import java.lang.annotation.Target;
 /**
  * <p>Created: 01-02-2019</p>
  * <p>author: Runi</p>
+ *
+ * <p>
+ *     Creates a partial class of the annotated class,
+ *     containing a getter to get an instance of the annotated class with
+ *     the current instance values.
+ * </p>
+ * <p>
+ *  use 'name' to set the name of the partial class.
+ *  use 'includeFields' if partial class should only contain a small subset of fields
+ *  use 'excludeFields' if partial class should contain most fields with some exceptions.
+ * </p>
+ * <p>
+ *     NOTE: if any of the given fields in 'includeFields' or 'excludeFields'
+ *     do not exist in annotated class an error will be thrown.
+ *     Or if any of the fields do not have a setter a compile error will occur.
+ * </p>
  */
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.ANNOTATION_TYPE)
